78d5a4033a63fd9b4db98c6f5bbce21fbbc790bc
PDgaming <produnyadehingia12345@gmail.com>
2026-05-27 23:25:39 +0530

    removed redundant scripts

--- take_screenshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILENAME="$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png"
-grim -g "$(slurp)" "$FILENAME"
-if [ -f "$FILENAME" ]; then
-  cat "$FILENAME" | wl-copy -t image/png
-fi
